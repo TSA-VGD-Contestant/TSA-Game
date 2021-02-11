dc41193751214d0e3303cb730e68d52e1ba261dd
Lars Schneider <Lars.sschneider@outlook.com>
2021-02-10 17:42:16 -0800

    Refactored biomes TODO:Noise

--- Entities/Player.cs
+++ Entities/Player.cs
@@ -4,291 +4,114 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class Player : BaseEntity
+class Player
 {
-    public enum WALKINGSTATES
+    //Player Texture
+    //TODO: Player Animation
+    private readonly Texture texture = Engine.LoadTexture("PlayerForward.png");
+
+    private readonly float ScreenX;
+    private float ScreenY;
+    private float Offset;
+    private readonly float Width = 64;
+    private readonly float Height = 64;
+
+    //Movement variables
+    private const float ACCELX = 1f;
+    private const float MAXVELX = 5f;
+    private float VelocityX;
+    private float VelocityY;
+
+    //Movement states
+    //Walk state
+    public enum WSTATE
     {
+        STANDING,
         LEFT,
-        RIGHT,
-        STANDING
+        RIGHT
+    };
+
+    private WSTATE WalkState;
+
+    public Player()
+    {
+        ScreenX = Game.Resolution.X / 2 - Width / 2;
+        ScreenY = Game.Resolution.Y / 2 - Height / 2;
+        Offset = -32;
+
+        WalkState = WSTATE.STANDING;
     }
 
-    private WALKINGSTATES WalkingState;
-
-    public enum JUMPINGSTATES
+    public void Input()
     {
-        JUMPING,
-        FALLING,
-        STANDING
-    }
-
-    private JUMPINGSTATES JumpingState;
-
-
-    //Movement constants
-    public readonly float MAXVELX = 3f;
-    public readonly float ACCELX = 0.5f;
-    public readonly float DEACCELX = 0.25f;
-
-    public readonly float GRAVACCEL = 0.5f;
-    public readonly float MAXGRAV = 7.5f;
-    public readonly float JUMPACCELY = 1.5f;
-    public readonly float MAXJUMPVEL = 7.5f;
-
-    private readonly Vector2 PositionOnScreen;
-
-    //Raygun
-    private RayGun raygun;
-    private bool raygunEquipped;
-    private const uint MAXAMMO = 6;
-    private uint ammo;
-    private bool shooting;
-
-    private List<Projectile> projectiles;
-
-    private readonly Texture ammoIndicator = Engine.LoadTexture("Ammo.png");
-
-    private readonly Sound Lasor = Engine.LoadSound("PlayerLasor.mp3");
-
-    public Player(Bounds2 bounds, Texture texture) : base(bounds, texture)
-    {
-        PositionOnScreen = bounds.Position;
-        JumpingState = JUMPINGSTATES.FALLING;
-
-        raygun = new RayGun(Engine.LoadTexture("RayGun.png"), 10);
-        raygunEquipped = false;
-
-        ammo = 6;
-
-        shooting = false;
-
-        projectiles = new List<Projectile>();
-    }
-
-    public override void Input()
-    {
-        if(Engine.GetKeyDown(Key.Space))
+        if(Engine.GetKeyHeld(Key.A))
         {
-            shooting = true;
+            WalkState = WSTATE.LEFT;
+        }
+        else if(Engine.GetKeyHeld(Key.D))
+        {
+            WalkState = WSTATE.RIGHT;
         }
         else
         {
-            shooting = false;
-        }
-        if (Engine.GetKeyDown(Key.R))
-        {
-            if(raygunEquipped)
-            {
-                ammo = MAXAMMO;
-            }
-        }
-
-        if(Engine.GetKeyDown(Key.Q))
-        {
-            raygunEquipped = !raygunEquipped;
-        }
-        if(Engine.GetKeyHeld(Key.A))
-        {
-            WalkingState = WALKINGSTATES.LEFT;
-        }
-        if(Engine.GetKeyHeld(Key.D))
-        {
-            WalkingState = WALKINGSTATES.RIGHT;
-        }
-        if(Engine.GetKeyHeld(Key.A) == Engine.GetKeyHeld(Key.D))
-        {
-            WalkingState = WALKINGSTATES.STANDING;
-        }
-
-        if(Engine.GetKeyHeld(Key.W))
-        {
-            if(JumpingState == JUMPINGSTATES.STANDING)
-            {
-                JumpingState = JUMPINGSTATES.JUMPING;
-            }
+            WalkState = WSTATE.STANDING;
         }
     }
 
-    public override void Update()
+    public void Update()
     {
-        switch (WalkingState)
-        {
-            case WALKINGSTATES.LEFT:
-                if (velocity.X - ACCELX >= MAXVELX)
-                {
-                    velocity.X -= ACCELX;
-                }
-                else
-                {
-                    velocity.X = -MAXVELX;
-                }
-                break;
-            case WALKINGSTATES.RIGHT:
-                if (velocity.X + ACCELX <= MAXVELX)
-                {
-                    velocity.X += ACCELX;
-                }
-                else
-                {
-                    velocity.X = MAXVELX;
-                }
-                break;
-            case WALKINGSTATES.STANDING:
-                if (velocity.X > 0)
-                {
-                    if (velocity.X - DEACCELX >= 0)
-                    {
-                        velocity.X -= DEACCELX;
-                    }
-                    else if(velocity.X + DEACCELX <= 0)
-                    {
-                        velocity.X += DEACCELX;
-                    }
-                    else
-                    {
-                        velocity.X = 0;
-                    }
-                }
-                else if (velocity.X < 0)
-                {
-                    if (velocity.X + DEACCELX <= 0)
-                    {
-                        velocity.X += DEACCELX;
-                    }
-                    else
-                    {
-                        velocity.X = 0;
-                    }
-                }
-                break;
-        }
-
-        switch (JumpingState)
-        {
-            case JUMPINGSTATES.JUMPING:
-                if (velocity.Y == -MAXJUMPVEL)
-                {
-                    JumpingState = JUMPINGSTATES.FALLING;
-                }
-                if (velocity.Y - JUMPACCELY >= MAXJUMPVEL)
-                {
-                    velocity.Y -= JUMPACCELY;
-                }
-                else
-                {
-                    velocity.Y = -MAXJUMPVEL;
-                }
-                break;
-            case JUMPINGSTATES.FALLING:
-                if (velocity.Y + GRAVACCEL <= MAXGRAV)
-                {
-                    velocity.Y += GRAVACCEL;
-                }
-                else
-                {
-                    velocity.Y = GRAVACCEL;
-                }
-                break;
-            case JUMPINGSTATES.STANDING:
-                velocity.Y = 0;
-                break;
-        }
-        bounds.Position += velocity;
-
-        if(shooting && raygunEquipped && ammo > 0)
-        {
-            ammo --;
-            if(raygun.IsFlipped())
-            {
-                projectiles.Add(new Projectile(Engine.LoadTexture("Projectile.png"), new Vector2(bounds.Position.X, bounds.Position.Y + bounds.Size.Y / 2 - 10), (raygun.IsFlipped()) ? -1 : 1, 10));
-            }
-            else
-            {
-                projectiles.Add(new Projectile(Engine.LoadTexture("Projectile.png"), new Vector2(bounds.Position.X + bounds.Size.X, bounds.Position.Y + bounds.Size.Y / 2 - 10), (raygun.IsFlipped()) ? -1 : 1, 10));
-            }
-            Engine.PlaySound(Lasor);
-        }
+        Movement();
     }
 
     public void Render()
     {
-        for (int i = 0; i < ammo; i++)
+        Engine.DrawTexture(texture, new Vector2(ScreenX, ScreenY));
+    }
+
+    private void Movement()
+    {
+        switch (WalkState)
         {
-            Engine.DrawTexture(ammoIndicator, new Vector2(Game.Resolution.X - 32 - (i * 50), Game.Resolution.Y - 32));
+            case WSTATE.LEFT:
+                if (VelocityX - ACCELX >= -MAXVELX)
+                {
+                    VelocityX -= ACCELX;
+                }
+                else
+                {
+                    VelocityX = -MAXVELX;
+                }
+                break;
+            case WSTATE.RIGHT:
+                if (VelocityX + ACCELX <= MAXVELX)
+                {
+                    VelocityX += ACCELX;
+                }
+                else
+                {
+                    VelocityX = MAXVELX;
+                }
+                break;
+            case WSTATE.STANDING:
+                VelocityX = 0;
+                break;
         }
 
-        Engine.DrawTexture(texture, bounds.Position);
-
-        if(raygunEquipped)
-        {
-            switch (WalkingState)
-            {
-                case WALKINGSTATES.RIGHT:
-                    RaygunRight();
-                    break;
-                case WALKINGSTATES.LEFT:
-                    RaygunLeft();
-                    break;
-                case WALKINGSTATES.STANDING:
-                    if (raygun.IsFlipped())
-                    {
-                        RaygunLeft();
-                    }
-                    else
-                    {
-                        RaygunRight();
-                    }
-                    break;
-            }
-        }
-        foreach (Projectile p in projectiles)
-        {
-            p.Render();
-        }
-
-
-
-        
+        Offset += VelocityX;
     }
 
-    private void RaygunLeft()
+    public bool HitsBounds(Bounds2 other)
     {
-        raygun.Render(new Vector2(bounds.Position.X - bounds.Size.X / 3 + 10, bounds.Position.Y + bounds.Size.Y / 4), TextureMirror.Horizontal);
+        return new Bounds2(ScreenX, ScreenY, Width, Height).Overlaps(other);
     }
 
-    private void RaygunRight()
+    public Bounds2 GetBounds()
     {
-        raygun.Render(new Vector2((float)(bounds.Position.X + (0.75 * bounds.Size.X)), bounds.Position.Y + bounds.Size.Y / 4), TextureMirror.None);
+        return new Bounds2(ScreenX, ScreenY, Width, Height);
     }
 
-    public override void Render(Vector2 offset)
+    public float GetOffset()
     {
-
-    }
-
-    public Vector2 GetPositionOnScreen()
-    {
-        return PositionOnScreen;
-    }
-
-    public WALKINGSTATES GetWalkingState()
-    {
-        return WalkingState;
-    }
-
-    public JUMPINGSTATES GetJumpingState()
-    {
-        return JumpingState;
-    }
-
-    public void SetWalkingState(WALKINGSTATES state)
-    {
-        WalkingState = state;
-    }
-
-    public void SetJumpingState(JUMPINGSTATES state)
-    {
-        JumpingState = state;
+        return Offset;
     }
 }
-
---
+++ Game/GrassBiome.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class GrassBiome
+{
+    private List<List<int>> Terrain;
+
+    private const int AIR = 0,
+                      DIRT = 1,
+                      GRASS = 2;         
+
+    public static readonly int LEVELONEWIDTH = 100;
+
+    private const int TILESIZE = 32;
+
+    private readonly Bounds2 screen = new Bounds2(0, 0, Game.Resolution.X, Game.Resolution.Y);
+
+    public GrassBiome()
+    {
+        InitTerrain();
+    }
+
+    public void Update(Player Player)
+    {
+        for (int y = 0; y < Terrain.Count; y++)
+        {
+            for (int x = 0; x < Terrain[y].Count; x++)
+            {
+                
+                int type = Terrain[y][x];
+                Bounds2 tileBounds = new Bounds2(((x - LEVELONEWIDTH / 2) * TILESIZE) - Player.GetOffset(), y * TILESIZE, TILESIZE, TILESIZE);
+                
+                if(screen.Overlaps(tileBounds))
+                {
+                    switch (type)
+                    {
+                        case AIR:
+                            Engine.DrawRectSolid(tileBounds, Color.Blue);
+                            Engine.DrawRectEmpty(tileBounds, Color.Black);
+                            break;
+                        case DIRT:
+                            Engine.DrawRectSolid(tileBounds, Color.Brown);
+                            Engine.DrawRectEmpty(tileBounds, Color.Black);
+                            break;
+                        case GRASS:
+                            Engine.DrawRectSolid(tileBounds, Color.Green);
+                            Engine.DrawRectEmpty(tileBounds, Color.Black);
+                            break;
+                    }
+                }
+
+            }
+        }
+    }
+
+    private void InitTerrain()
+    {
+        Terrain = new List<List<int>>((int)(Game.Resolution.Y / TILESIZE));
+        Terrain.Add(new List<int>());
+        for(int y = 0; y < (int)(Game.Resolution.Y / TILESIZE); y++)
+        {
+            Terrain.Add(new List<int>(LEVELONEWIDTH));
+            for(int x = 0; x < LEVELONEWIDTH; x++)
+            {
+                if (y <= 10)
+                {
+                    Terrain[y].Add(AIR);
+                }
+                else if (y <= 11)
+                {
+                    Terrain[y].Add(GRASS);
+                }
+                else
+                {
+                    Terrain[y].Add(DIRT);
+                }
+            }
+        }
+    }
+}
